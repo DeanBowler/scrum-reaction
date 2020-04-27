8cb9f4bd31f05b5105cf2526bde910e7983536ac
Dean Bowler <dean.bowler@gmail.com>
2020-04-27 23:11:28 +0100

    Throw confetti when consensus is met!

--- graphql/planning-poker/SessionStats.tsx
+++ graphql/planning-poker/SessionStats.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import { Poker_Session } from '../../generated/graphql';
+import React, { useLayoutEffect, useRef } from 'react';
+import { pipe, map, filter, min, max, mean, median, reduce, all } from 'ramda';
+
+import CanvasConfetti from 'canvas-confetti';
+
+import { Poker_Session, Poker_User_Session } from '../../generated/graphql';
 import Box from '../../styled/Box';
-import { pipe, map, filter, min, max, mean, median, reduce } from 'ramda';
+import Text from '../../styled/Text';
 import Card from '../../components/Card';
 
 export default function SessionStats(session: Poker_Session) {
-  const votes = session.user_sessions.map(s => s.current_vote);
+  const votes = pipe(
+    map((s: Poker_User_Session) => s.current_vote),
+    filter(v => !!v),
+  )(session.user_sessions);
 
-  const unknownVotes = filter(v => v === '?')(votes).length;
+  // session.user_sessions.map(s => s.current_vote);
+
+  const unknownVotes = votes.length ? filter(v => v === '?')(votes).length : undefined;
 
   const numericVotes = pipe(
     map(Number.parseInt),
@@ -25,8 +34,40 @@ export default function SessionStats(session: Poker_Session) {
   const meanVote = numericVotes.length ? mean(numericVotes) : undefined;
   const medianVote = numericVotes.length ? median(numericVotes) : undefined;
 
+  const areAllVotesNumeric = numericVotes.length == votes.length;
+  const consensusReached =
+    areAllVotesNumeric &&
+    numericVotes.length > 1 &&
+    all(x => x === numericVotes[0], numericVotes);
+
+  const consensusTextRef = useRef<HTMLDivElement>();
+
+  useLayoutEffect(() => {
+    if (consensusReached) {
+      setTimeout(() => {
+        const consensusTextEl = consensusTextRef.current;
+        const textCenterX = consensusTextEl.offsetLeft + consensusTextEl.offsetWidth / 2;
+
+        CanvasConfetti({
+          particleCount: 100,
+          startVelocity: 45,
+          spread: 70,
+          origin: {
+            y: consensusTextEl.offsetTop / window.innerHeight,
+            x: textCenterX / window.innerWidth,
+          },
+        });
+      }, 1000);
+    }
+  }, [consensusReached]);
+
   return (
     <Card ml={[1, 2]} title="Stats" flex="0 0" minWidth={[6, , 7]}>
+      {consensusReached && (
+        <Text as="div" mb="3" ref={consensusTextRef} fontWeight="bold">
+          CONSENSUS
+        </Text>
+      )}
       <Box>Highest: {highestVote}</Box>
       <Box>Lowest: {lowestVote}</Box>
       <Box>Mean: {meanVote}</Box>
