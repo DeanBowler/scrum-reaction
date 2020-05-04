aac0b1366eae84ff8cb1158b3a7426e999755e3a
Dean Bowler <dean.bowler@gmail.com>
2020-05-05 00:09:40 +0100

    Add spacing variants for Card component
    - Added the narrow and cosy variants for the card component for differing amounts of padding

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,23 +6,42 @@ import Text from '@styled/Text';
 import Flex from '@styled/Flex';
 import styled, { css } from 'styled-components';
 import { getColor } from '@styled/theme';
-import { ResponsiveValue, FlexProps } from 'styled-system';
+import { ResponsiveValue, FlexProps, variant } from 'styled-system';
 
 type Variants = 'link';
+
+type SpacingVariants = 'narrow' | 'cosy';
 
 export interface CardProps extends BorderBoxProps {
   children: React.ReactNode;
   className?: string;
   title?: string;
   variant?: ResponsiveValue<Variants>;
+  spacingVariant?: ResponsiveValue<SpacingVariants>;
 }
 
-const CardContentContainer = styled(BorderBox)`
+interface CardContentContainerProps extends BorderBoxProps {
+  spacingVariant?: ResponsiveValue<SpacingVariants>;
+}
+
+const CardContentContainer = styled(BorderBox)<CardContentContainerProps>`
   background-color: ${getColor('neutralLightest')};
   border: 1px solid ${getColor('neutralMid')};
   border-radius: 5px;
   box-shadow: 0px 5px 0px -1px rgba(0, 0, 0, 0.1);
   transition: box-shadow 100ms;
+
+  ${variant({
+    prop: 'spacingVariant',
+    variants: {
+      narrow: {
+        padding: [1, 2],
+      },
+      cosy: {
+        padding: [2, 3],
+      },
+    },
+  })}
 `;
 
 const CardTitle = styled(Text)`
@@ -39,11 +58,18 @@ interface CardContainerProps extends FlexProps {
 const CardContainer = styled(Flex)<CardContainerProps>`
   color: ${getColor('neutralDark')};
   text-decoration: none;
+  transform: translateY(0);
+  transition: transform 200ms;
+  background: transparent;
+  border: none;
 
   ${p =>
     p.variant === 'link' &&
     css`
       cursor: pointer;
+      :hover {
+        transform: translateY(-3px);
+      }
       :hover > ${CardTitle} {
         color: ${getColor('primary')};
       }
@@ -62,26 +88,28 @@ const CardContainer = styled(Flex)<CardContainerProps>`
     `};
 `;
 
-const Card = React.forwardRef(({ title, children, variant, ...rest }: CardProps, ref) => {
-  return (
-    <CardContainer
-      ref={ref}
-      tabIndex={variant === 'link' ? 0 : undefined}
-      variant={variant}
-      flexDirection="column"
-      flex="1 1 auto"
-      {...rest}
-    >
-      {title && (
-        <CardTitle as="div" pb={[1]} fontSize={[2, 3]}>
-          {title}
-        </CardTitle>
-      )}
-      <CardContentContainer padding={[3, 4]} mb="5px">
-        <Box width="100%">{children}</Box>
-      </CardContentContainer>
-    </CardContainer>
-  );
-});
+const Card = React.forwardRef(
+  ({ title, children, variant, spacingVariant, ...rest }: CardProps, ref) => {
+    return (
+      <CardContainer
+        ref={ref}
+        tabIndex={variant === 'link' ? 0 : undefined}
+        variant={variant}
+        flexDirection="column"
+        flex="1 1 auto"
+        {...rest}
+      >
+        {title && (
+          <CardTitle as="div" pb={[1]} fontSize={[2, 3]}>
+            {title}
+          </CardTitle>
+        )}
+        <CardContentContainer spacingVariant={spacingVariant} padding={[3, 4]}>
+          <Box width="100%">{children}</Box>
+        </CardContentContainer>
+      </CardContainer>
+    );
+  },
+);
 
 export default Card;
